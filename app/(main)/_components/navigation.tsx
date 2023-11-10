3f667b48eee50bbde5f03ab730c9d330871c6a59
Jose Manuel <josemanuel.silva.jmst@gmail.com>
2023-11-09 23:15:40 -0600

    [add] search functionality

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -21,6 +21,7 @@ import {
   PopoverTrigger,
   PopoverContent,
 } from "@/components/ui/popover";
+import { useSearch } from "@/hooks/use-search";
 
 import { UserItem } from "./user-item";
 import { Item } from "./item";
@@ -29,6 +30,7 @@ import { TrashBox } from "./trash-box";
 
 export const Navigation = () => {
   const router = useRouter();
+  const search = useSearch();
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)");
   const create = useMutation(api.documents.create);
@@ -151,7 +153,7 @@ export const Navigation = () => {
             label="Search"
             icon={Search}
             isSearch
-            onClick={() => {}}
+            onClick={search.onOpen}
           />
           <Item
             label="Settings"
