d9b0f828c91a8f38c9663ef61d5991eff32b5700
Jose Manuel <josemanuel.silva.jmst@gmail.com>
2025-07-15 18:22:07 -0600

    update: light/dark themes, align BlockNote styling; fix Navigation z-index

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -137,7 +137,7 @@ export const Navigation = () => {
       <aside
         ref={sidebarRef}
         className={cn(
-          "group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-99999",
+          "group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-999",
           isResetting && "transition-all ease-in-out duration-300",
           isMobile && "w-0"
         )}
